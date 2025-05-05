2a9badadb01dda3cb06c6540552f051115734258
hafdisarnag <hafdisg24@ru.is>
2025-05-05 16:36:32 +0000

    property a linkinnum /proprty og med id /property/id

--- property/urls.py
+++ property/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     # http://localhost:9000 (root)
-    path('', views.index, name='property-index'),
-    path('<int:id>', views.get_property_by_id, name='property-by-id'),
+    path('property/', views.index, name='property-index'),
+    path('property/<int:id>', views.get_property_by_id, name='property-by-id'),
 ]
