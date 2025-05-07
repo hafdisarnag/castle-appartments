e971757f182793827b0f12e5cb65fd2d568d41cb
hafdisarnag <hafdisg24@ru.is>
2025-05-07 23:01:38 +0000

    search

--- property/views.py
+++ property/views.py
@@ -1,10 +1,27 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from property.models import Property
 from sellers.models import Seller
 
 
 def index(request):
+    if 'search_filter' in request.GET:
+        return JsonResponse({
+            'data': [{
+                'type': x.type,
+                'address': x.address,
+                'zip': x.zip,
+                'city': x.city,
+                'size': x.size,
+                'rooms': x.rooms,
+                'bathrooms': x.bathrooms,
+                'bedrooms': x.bedrooms,
+                'price': x.price,
+                'id': x.id,
+                'image': x.images.first().image_url if x.images.exists() else None,
+
+            } for x in Property.objects.filter(address__icontains=request.GET['search_filter']) .order_by('id')],
+        })
     return render(request,"property/property.html",  {
         "properties": Property.objects.all(),
     })
