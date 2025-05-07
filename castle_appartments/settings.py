c0b09a0601728fdaeafa55a88b1aca4aacdba2ca
Embla123 <sigridurj24@ru.is>
2025-05-07 14:11:37 +0000

    user app

--- castle_appartments/settings.py
+++ castle_appartments/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'property.apps.PropertiesConfig',
+    'user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
--- castle_appartments/urls.py
+++ castle_appartments/urls.py
@@ -24,9 +24,10 @@ urlpatterns = [
     path('', include('home.urls')),
     path('', include('property.urls')),
     path('',include('offers.urls')),
-    path('', include("users.urls")),
 
     path('property/<int:id>', property.views.get_property_by_id),
 
     path('admin/', admin.site.urls),
+
+    path('user/', include('user.urls')),
 ]
