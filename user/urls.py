fa351e24a886b734331c6153c11f7b77fc675378
Embla123 <sigridurj24@ru.is>
2025-05-07 20:34:19 +0000

    login og logout

--- user/urls.py
+++ user/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('register', views.register, name='register'),
     path('login', LoginView.as_view(template_name='user/login.html'), name='login'),
-    path('logout', LogoutView.as_view(), name='logout'),
+    path('logout', LogoutView.as_view(next_page='home-index'), name='logout'),
+    path('profile', views.profile, name='profile'),
 ]
