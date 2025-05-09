bdc24b4fbe4b97b934a6552cb336d5f300026dd7
solliljaha <solliljaha@gmail.com>
2025-05-09 13:04:06 +0000

    inspiration

--- inspiration/models.py
+++ inspiration/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 
-# Create your models here.
 from django.db import models
 
 class DesignIdea(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    image_url = models.URLField()
+    image = models.ImageField(upload_to='design_ideas/', blank=True, null=True)
     category = models.CharField(max_length=100, choices=[
         ('living', 'Living Room'),
         ('bedroom', 'Bedroom'),
@@ -17,3 +16,4 @@ class DesignIdea(models.Model):
 
     def __str__(self):
         return self.title
+
