f25dc88f5bad71cb05bb46e6876f58ddbfff7faa
solliljaha <solliljaha@gmail.com>
2025-05-13 10:47:51 +0000

    real estate agencies home

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from property.models import Property
+from sellers.models import Seller  # Ef Seller er í öðru appi
 from openhouses.views import get_upcoming_open_houses
 
 def index(request):
@@ -7,4 +8,5 @@ def index(request):
         "properties": Property.objects.all(),
         "featured_properties": Property.objects.order_by('-date')[:3],
         "open_houses": get_upcoming_open_houses(),
-})
+        "agencies": Seller.objects.filter(seller_type="agency"),
+    })
